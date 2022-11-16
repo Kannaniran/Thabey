fcaf417738605342617b43263d3b0f8c875ee657
kannabiran.B kanna <killerkanna2019@outlook.com>
2022-11-16 22:50:04 +0530

    Delete method

--- CarterPlus/Controllers/LoginAPIController.cs
+++ CarterPlus/Controllers/LoginAPIController.cs
@@ -1,13 +1,45 @@
-﻿using System;
+﻿using CarterPlus.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+
 namespace CarterPlus.Controllers
 {
     public class LoginAPIController : ApiController
     {
+        LoginModel[] ObjLogin = new LoginModel[]
+       {
+            new LoginModel { ID = 1, Name = "Test1"},
+            new LoginModel { ID = 1, Name = "Test2"},
+            new LoginModel { ID = 1, Name = "Test3"},
+       };
+
+        public IEnumerable<LoginModel> GetAllEmployees()
+        {
+            return ObjLogin;
+        }
+
+        public IHttpActionResult GetEmployee(int id)
+        {
+            var LoginDetails = ObjLogin.FirstOrDefault((p) => p.ID == id);
+            if (LoginDetails == null)
+            {
+                return NotFound();
+            }
+            return Ok(LoginDetails);
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var LoginDetails = ObjLogin.FirstOrDefault((p) => p.ID == id);
+            if (LoginDetails == null)
+            {
+                return NotFound();
+            }
+            return Ok(LoginDetails);
+        }
     }
 }
---
+++ CarterPlus/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarterPlus.Models
+{
+    public class LoginModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime Password { get; set; }
+    }
+}
