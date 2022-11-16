c75246baf8724fe4cfad9c379f552edc7d4ea300
kannabiran.B kanna <killerkanna2019@outlook.com>
2022-11-16 22:54:00 +0530

    Eit and fetch

--- CarterPlus/Controllers/LoginAPIController.cs
+++ CarterPlus/Controllers/LoginAPIController.cs
@@ -41,5 +41,23 @@ namespace CarterPlus.Controllers
             }
             return Ok(LoginDetails);
         }
+        public IHttpActionResult Edit(int id)
+        {
+            var LoginDetails = ObjLogin.FirstOrDefault((p) => p.ID == id);
+            if (LoginDetails == null)
+            {
+                return NotFound();
+            }
+            return Ok(LoginDetails);
+        }
+        public IHttpActionResult Details(int id)
+        {
+            var LoginDetails = ObjLogin.FirstOrDefault((p) => p.ID == id);
+            if (LoginDetails == null)
+            {
+                return NotFound();
+            }
+            return Ok(LoginDetails);
+        }
     }
 }
